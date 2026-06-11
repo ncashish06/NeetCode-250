52d67c6f62bde90e941a568d1a7c2a41f18d1dda
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-06-11 15:00:06 +0200

    Time: 0 ms (100%), Space: 20.4 MB (24.98%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Date Solved: 11 June 2026, Thursday
+    # Refer: codestorywithMIK
+    # Blind 75
+    # This code used in 11 June 2026 POTD, LC. 3558 Number of Ways to Assign Edge Weights I
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # Bottom up recursion approach
+        if not root:
+            return 0
+        left_max_depth = self.maxDepth(root.left)
+        right_max_depth = self.maxDepth(root.right)
+        return 1 + max(left_max_depth, right_max_depth)
+        """
+        # Top down recursion approach
+        if not root:
+            return 0
+        self.max_depth = 0
+        def traversal(curr_node, depth):
+            self.max_depth = max(self.max_depth, depth)
+            if curr_node.left:
+                traversal(curr_node.left, depth + 1)
+        
+            if curr_node.right:
+                traversal(curr_node.right, depth + 1)
+        
+        traversal(root, 1)
+        return self.max_depth
+        """
