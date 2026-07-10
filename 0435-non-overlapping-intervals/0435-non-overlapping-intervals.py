20060ec12f1f0635f4f4d89af70369a90323d88d
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-10 13:56:49 +0200

    Time: 75 ms (64.46%), Space: 49 MB (55.32%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,42 +3,30 @@ class Solution:
     # Blind 75
     # Time: O(nlogn) for sorting, Space: O(1)
     def eraseOverlapIntervals(self, arr: List[List[int]]) -> int:
-        # Approach 1: sort by end
-        # Refer: Namaste DSA
+        """
+        # Approach 1: Sort by end
+        # Refer: NeetCode, Namaste DSA
         arr.sort(key=lambda x: x[1])
         remove_count = 0
-        k = float("-inf")
-        for i in range(len(arr)):
+        prev_end = arr[0][1]
+        for i in range(1, len(arr)):
             start, end = arr[i][0], arr[i][1]
-            if start < k:  # Golden rule to check overlap: current_start <= last_end
+            if (start < prev_end):  # Golden rule to check overlap: current_start <= last_end
                 remove_count += 1
             else:
-                k = end
+                prev_end = end
         return remove_count
         """
-        # Approach 2: sort by start
-        # Refer: codestorywithMIK
+        # Approach 2: Sort by start
+        # Refer: NeetCode
         arr.sort(key=lambda x: x[0])
-        n = len(arr)
-        count = 0
-        i, j = 0, 1
-
-        while j < n:
-            curr_start, curr_end = arr[i][0], arr[i][1]
-            next_start, next_end = arr[j][0], arr[j][1]
-
-            if curr_end <= next_start:  # SAFE — no overlap
-                i = j
-                j += 1
-            elif curr_end <= next_end:
-                # GREEDY: remove next interval, it has high chance of overlapping with future intervals
-                j += 1
-                count += 1
-            else:  # curr_end > next_end
-                # GREEDY: remove current interval, it has high chance of overlapping with future intervals
-                i = j
-                j += 1
-                count += 1
-
-        return count
-        """
+        remove_count = 0
+        prev_end = arr[0][1]
+        for i in range(1, len(arr)):
+            start, end = arr[i][0], arr[i][1]
+            if (start < prev_end):  # Golden rule to check overlap: current_start <= last_end
+                remove_count += 1
+                prev_end = min(end, prev_end)  # GREEDY: Keep only the interval which ends early as said by NamasteDSA
+            else:
+                prev_end = end
+        return remove_count
