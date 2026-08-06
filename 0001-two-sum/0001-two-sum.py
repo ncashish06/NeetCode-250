3e1758532b639415ea62b169c8eccc84a9b2cee4
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-08-06 19:45:27 +0200

    Time: 0 ms (100%), Space: 20.5 MB (40.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,23 @@
 class Solution:
-    # Date Solved: 16 April 2026, Thursday
+    # Date Solved: 6 August 2026, Thursday
     # Blind 75
     # Refer: structy.net
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        # For Sorted Array: Two-pointer approach.
+        # Time: O(n), Space: O(1)
+        left, right = 0, len(nums) - 1
+        while left < right:
+            current_sum = nums[left] + nums[right]
+            if current_sum == target:
+                return [left, right]
+            elif current_sum < target:
+                left += 1
+            else:
+                right -= 1
+        return []
+        """
+        # For Unsorted Array: Time: O(n), Space: O(n)
         previous_nums = {}
 
         for index, num in enumerate(nums):
