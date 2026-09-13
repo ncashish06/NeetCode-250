a1492315bac755990df14365def46ff5e29a928a
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-09-12 22:59:56 -0700

    Time: 51 ms (85.03%), Space: 29.5 MB (88.24%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,33 +1,34 @@
 class Solution:
     # Date Solved: 12 August 2026, Wednesday
     # Blind 75
-    # Refer: codestorywithMIK for optimal and NeetCode for brute-force and optimal
+    # Refer: B2Go, codestorywithMIK
     def maxArea(self, height: List[int]) -> int:
         """
         # Approach 1: Brute force
         # Time: O(n^2), Space: O(1)
-        res = 0
-        for i in range(len(height)):
-            for j in range(i + 1, len(height)):
-                res = max(res, min(height[i], height[j]) * (j - i))
-        return res
+        max_water = 0
+        n = len(height)
+        for i in range(n):
+            for j in range(i + 1, n):
+                water = min(height[i], height[j]) * (j - i)
+                max_water = max(max_water, water)
+        return max_water
         """
         # Approach 2: Two Pointers, Greedy
         # Time: O(n), Space: O(1)
         n = len(height)
-        i, j = 0, n - 1
-        water = 0
+        left, right = 0, n - 1
+        max_water = 0
 
-        while i < j:
-            # start from the smallest one and calculate water
-            h = min(height[i], height[j])
-            w = j - i
-            area = h * w
-            water = max(water, area)
+        while left < right:
+            water = min(height[left], height[right]) * (right - left)
+            max_water = max(max_water, water)
 
-            if height[i] < height[j]:
-                i += 1
-            else:
-                j -= 1
-
-        return water
+            if height[left] < height[right]:
+                left += 1
+            elif height[left] > height[right]:
+                right -= 1
+            else:  # if both left and right have same height then move both pointers inwards
+                left += 1
+                right -= 1
+        return max_water
